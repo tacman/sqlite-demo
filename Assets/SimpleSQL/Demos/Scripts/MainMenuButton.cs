5500d4b65b2756bd95c4d5f35c5fc2fd9c7af93d
Tac Tacelosky <tacman@gmail.com>
2020-11-04 07:44:44 -0500

    initial version (loading simplesql package)

---
+++ Assets/SimpleSQL/Demos/Scripts/MainMenuButton.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleSQL.Demos
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using SimpleSQL;
+
+    public class MainMenuButton : MonoBehaviour
+    {
+        public SimpleSQLManager dbManager;
+
+        public void MainMenu()
+        {
+            dbManager.Close();
+            dbManager.Dispose();
+
+            SceneManager.LoadScene(0);
+        }
+    }
+}
